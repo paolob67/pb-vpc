f0c27e6aa112996daf97f1e8a976c86ec53dfeb4
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-06 14:45:40 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,12 @@
 ##############################################################################
 # VPC definition
 ##############################################################################
+data "ibm_resource_group" "all_rg" {
+  name = var.resource_group_name
+}
 
 resource "ibm_is_vpc" "vpc" {
-  name                      = var.vpc_name
-  resource_group            = var.resource_group_name
+  name                      = var.unique_id
+  resource_group            = data.ibm_resource_group.all_rg.id
+  # address_prefix_management = "manual"
 }
