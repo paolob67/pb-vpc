bd6f19cd55a2713121dbbcc882bab5b72b991919
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-06 15:04:44 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -4,7 +4,7 @@
 ##############################################################################
 
 ##############################################################################
-# VPC definition
+# Create VPC 
 ##############################################################################
 data "ibm_resource_group" "all_rg" {
   name = var.resource_group_name
@@ -14,4 +14,21 @@ resource "ibm_is_vpc" "vpc" {
   name                      = var.vpc_name
   resource_group            = data.ibm_resource_group.all_rg.id
   # address_prefix_management = "manual"
+}
+
+##############################################################################
+# Create Subnets
+##############################################################################
+
+resource "ibm_is_subnet" "management_subnet" {
+  vpc             = ibm_is_vpc.vpc.id
+}
+
+resource "ibm_is_subnet" "frontend_subnet" {
+  vpc             = ibm_is_vpc.vpc.id
+  public_gateway = ibm_is_public_gateway.repo_gateway[count.index].id
+}
+
+resource "ibm_is_subnet" "backend_subnet" {
+  vpc             = ibm_is_vpc.vpc.id
 }
