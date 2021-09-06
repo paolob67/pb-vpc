388e4e39ce97da3a588b966dd84414b71355efad
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-06 16:54:15 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -6,6 +6,7 @@
 ##############################################################################
 # Create VPC 
 ##############################################################################
+# get the resource group id from the passed resource group name
 data "ibm_resource_group" "rg" {
   name = var.resource_group_name
 }
@@ -54,7 +55,7 @@ resource "ibm_is_subnet" "backend_subnet" {
 }
 
 ##############################################################################
-# Create Load Balancer
+# Create Load Balancer for the front-end subnet
 ##############################################################################
 resource "ibm_is_lb" "load_balancer" {
   name           = "load-balancer"
@@ -62,7 +63,42 @@ resource "ibm_is_lb" "load_balancer" {
   subnets        = [ ibm_is_subnet.frontend_subnet.id ]
   resource_group = data.ibm_resource_group.rg.id
   timeouts {
-    create = "15m"
-    delete = "15m"
+    create       = "15m"
+    delete       = "15m"
   }
+  depends_on     = [ ibm_is_subnet.frontend_subnet ]
 }
+
+resource "ibm_is_lb_pool" "back_end_pool" {
+  lb                 = ibm_is_lb.load_balancer.id
+  name               = "back-end-pool"
+  protocol           = "http"
+  algorithm          = "round_robin"
+  health_delay       = "5"
+  health_retries     = "2"
+  health_timeout     = "2"
+  health_type        = "http"
+  health_monitor_url = "/"
+  depends_on         = [ ibm_is_lb.load_balancer ]
+}
+
+resource "ibm_is_lb_listener" "front_end_listener" {
+  lb           = ibm_is_lb.load_balancer.id
+  port         = "80"
+  protocol     = "http"
+  default_pool = ibm_is_lb_pool.back_end_pool.id
+  depends_on   = [ ibm_is_lb_pool.back_end_pool ]
+}
+
+
+/* will attach servers to load balancer in the compute node... */
+/*
+resource "ibm_is_lb_pool_member" "webapptier-lb-pool-member-zone1" {
+  count          = var.frontend_count
+  lb             = ibm_is_lb.webapptier-lb.id
+  pool           = element(split("/", ibm_is_lb_pool.webapptier-lb-pool.id), 1)
+  port           = "8080"
+  target_address = ibm_is_instance.frontend-server[count.index].primary_network_interface[0].primary_ipv4_address
+  depends_on     = [ibm_is_lb_pool.webapptier-lb-pool]
+}
+*/
