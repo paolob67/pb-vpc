26d39bcf6807ad5194d06d1881c3ee09b3fc8938
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-07 14:47:14 +0200

    created security module

---
+++ security.tf
@@ -0,0 +1,15 @@
+##############################################################################
+# This module defines the three security groups 
+# Bastion, FrontEnd, BackEnd
+##############################################################################
+
+##############################################################################
+# Bastion
+# Allow SSH connections from outside (might want to restrict that) 
+##############################################################################
+# TODO: use to restrict from subnetipv4_cidr_block
+resource "ibm_is_security_group" "bastion_security_group" {
+  name = "bastion-security-group"
+  vpc  = ibm_is_vpc.ibm_is_vpc.vpc.id.id
+}
+
