64fc3897f9de53aefccdafc868bc53a62143d64d
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-07 15:18:02 +0200

    Update security.tf

--- security.tf
+++ security.tf
@@ -13,7 +13,7 @@ resource "ibm_is_security_group" "bastion_security_group" {
   vpc   = ibm_is_vpc.vpc.id
 }
 
-# open SSH port for all incoming connections
+# open SSH port for all incoming connections (could be restricetd to whitelist)
 resource "ibm_is_security_group_rule" "bastion_allow_ssh" {
   group     = ibm_is_security_group.bastion_security_group.id
   direction = "inbound"
@@ -24,3 +24,23 @@ resource "ibm_is_security_group_rule" "bastion_allow_ssh" {
   }
 }
 
+##############################################################################
+# FrontEnd
+# Allow TCP/8080 from outside (could be restriced to LB ip addresses)
+##############################################################################
+# TODO: use to restrict from subnetipv4_cidr_block
+resource "ibm_is_security_group" "frontend_security_group" {
+  name = "frontend-security-group"
+  vpc   = ibm_is_vpc.vpc.id
+}
+
+# open SSH port for all incoming connections (could be restricetd to whitelist)
+resource "ibm_is_security_group_rule" "frontend_allow_http" {
+  group     = ibm_is_security_group.bastion_security_group.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = 8080
+    port_max = 8080
+  }
+}
