3db2ee6d2fbbe04ae69ab2ca13a49e19dbbb9622
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-07 15:05:44 +0200

    Update security.tf

--- security.tf
+++ security.tf
@@ -13,3 +13,14 @@ resource "ibm_is_security_group" "bastion_security_group" {
   vpc   = ibm_is_vpc.vpc.id
 }
 
+# open SSH port for all incoming connections
+resource "ibm_is_security_group_rule" "bastion_allow_ssh" {
+  group     = ibm_is_security_group.bastion-security-group.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  tcp {
+    port_min = 22
+    port_max = 22
+  }
+}
+
