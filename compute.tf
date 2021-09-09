8eaf934e28207413f98e43ca8ebb02a3e11574ef
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-09 17:00:53 +0200

    adding frontend autoscaling

--- compute.tf
+++ compute.tf
@@ -41,3 +41,27 @@ resource "ibm_is_floating_ip" "bastion_floating_ip" {
   name   = "bastion-floating-ip"
   target = ibm_is_instance.bastion_host.primary_network_interface[0].id
 }
+
+##############################################################################
+# Create Front end server farm
+# Using this for EU-de
+#  - r010-3bef996a-0dd4-48cc-b857-fd03e8dfa0db   
+#    (ibm-centos-8-3-minimal-amd64-3 centos-8-amd64)
+##############################################################################
+# Define instance template
+resource "ibm_is_instance_template" "frontend_template" {
+  name    = "frontend-template"
+  image   = data.ibm_is_image.bastion_host_image.id
+  profile = "bx2-2x8"
+  primary_network_interface {
+    subnet          = ibm_is_subnet.frontend_subnet.id
+    security_groups = [ ibm_is_security_group.frontend_security_group.id ]
+  }
+  timeouts {
+    create          = "10m"
+    delete          = "10m"
+  }
+  zone              = "${var.ibm_region}-1"
+  resource_group    = data.ibm_resource_group.rg.id
+  keys              = [ data.ibm_is_ssh_key.ssh_key.id ]
+}
