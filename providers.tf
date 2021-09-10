9b27b24a182dd2011f8879d58c5c495a50b23e52
Paolo Bianchini <paolo.bianchini@gmail.com>
2021-09-10 11:01:32 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,15 +2,15 @@
 # Terraform Providers
 ##############################################################################
 
-# Uncomment this block when using with Terraform v 0.13 or higher
-# terraform {
-#   required_providers {
-#     ibm = {
-#       source = "IBM-Cloud/ibm"
-#       version = "1.31.0"
-#     }
-#   }
-# }
+Uncomment this block when using with Terraform v 0.13 or higher
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = "1.31.0"
+    }
+  }
+}
 
 ##############################################################################
 
